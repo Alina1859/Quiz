a362dca2780daf96a43846e1cb50594c457c7795
Gavrilov Grigoriy <lowww1337@gmail.com>
2025-11-13 12:23:18 +0300

    Добавит метрику

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import Script from 'next/script'
 import './colors.css'
 import './globals.css'
+import YandexMetrika from '@/components/YandexMetrika'
 
 const bebasNeue = localFont({
   src: [{ path: '../public/fonts/BebasNeue-Bold.ttf', weight: '700', style: 'normal' }],
@@ -75,18 +76,7 @@ export default function RootLayout({
       src={`https://www.google.com/recaptcha/api.js?render=${recaptchaSiteKey}`}
       strategy="afterInteractive"
     />
-    <Script>
-      {`function(){(m[i].a=m[i].a||[]).push(arguments)};         m[i].l=1*new Date();         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)     })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105288522', 'ym');      ym(105288522, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true}); `}
-    </Script>
-    <noscript>
-      <div>
-        <img
-          src={`https://mc.yandex.ru/watch/105288522`}
-          style={{ position: "absolute", left: "-9999px;" }}
-          alt=""
-        />
-      </div>
-    </noscript>
+    <YandexMetrika />
     {children}
     </body>
     </html>
